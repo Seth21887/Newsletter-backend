acdf56dc598017e5ca7be6ff707782a1fbb11b66
Seth21887 <101611145+Seth21887@users.noreply.github.com>
2022-09-26 17:11:47 -0700

    settings updates

--- cmbackend/settings.py
+++ cmbackend/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'cmbackend.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://newsletterbackendapi.herokuapp.com",
+    "https://newsletterfrontend.herokuapp.com/",
 ]
 
 TEMPLATES = [
